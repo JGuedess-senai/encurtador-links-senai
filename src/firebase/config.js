389b680f7921054f95d4e1f46f560acc841848ce
JGuedess-senai <joao.p.guedes8@aluno.senai.br>
2026-05-09 15:47:45 -0300

    Projeto adaptado para o vercel e tudo configurado certo

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZB8VfSe90M_JJoZe2GbZtk9j9gW3oQo4",
-  authDomain: "encurtador-links-senai-jd.firebaseapp.com",
-  projectId: "encurtador-links-senai-jd",
-  storageBucket: "encurtador-links-senai-jd.firebasestorage.app",
-  messagingSenderId: "482821299608",
-  appId: "1:482821299608:web:865be815bc58bb82e75fed"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
